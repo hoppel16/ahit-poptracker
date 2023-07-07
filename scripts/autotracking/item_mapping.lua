0d452db54801541c23d4d15a87dac39aca53eff8
subenji <8143051+subenji@users.noreply.github.com>
2023-07-07 10:48:39 +0100

    Autotracking for Mafia Town, Dead Bird Studio, DBS Basement, Subcon Forest Autotracking also tracks hats and settings
    Adjusted second Old Man section, as autotracking searches by unique names
    Added Mafia Town - Camera Badge 2 as it was not present
    Added DBS Basement - Area Completion as it was not present
    Added Yarn consumable-type item
    (This is used for Hat autotracking, but is not displayed. I added an icon anyway just in case)

---
+++ scripts/autotracking/item_mapping.lua
@@ -0,0 +1,114 @@
+-- use this file to map the AP item ids to your items
+-- first value is the code of the target item and the second is the item type (currently only "toggle", "progressive" and "toggle" but feel free to expand for your needs!)
+-- here are the SM items as an example: https://github.com/Cyb3RGER/sm_ap_tracker/blob/main/scripts/autotracking/item_mapping.lua
+ITEM_MAPPING = {
+    [300001] = {"yarn", "consumable"},
+
+    -- Relics
+    --[[[300006] = {"Relic (Burger Patty)", "toggle"},
+    [300007] = {"Relic (Burger Cushion)", "toggle"},
+    [300008] = {"Relic (Mountain Set)", "toggle"},
+    [300009] = {"Relic (Train)", "toggle"},
+    [300010] = {"Relic (UFO)", "toggle"},
+    [300011] = {"Relic (Cow)", "toggle"},
+    [300012] = {"Relic (Cool Cow)", "toggle"},
+    [300013] = {"Relic (Tin-foil Hat Cow)", "toggle"},
+    [300014] = {"Relic (Crayon Box)", "toggle"},
+    [300015] = {"Relic (Red Crayon)", "toggle"},
+    [300016] = {"Relic (Blue Crayon)", "toggle"},
+    [300017] = {"Relic (Green Crayon)", "toggle"},]]--
+
+    -- Badges
+    -- [300024] = {"Projectile Badge", "toggle"},
+    -- [300025] = {"Fast Hatter Badge", "toggle"},
+    -- [300026] = {"Hover Badge", "toggle"},
+    [300027] = {"hookshot", "toggle"},
+    -- [300028] = {"Item Magnet Badge", "toggle"},
+    -- [300029] = {"No Bonk Badge", "toggle"},
+    -- [300030] = {"Compass Badge", "toggle"},
+    [300031] = {"scooter", "toggle"},
+    -- [300043] = {"Badge Pin", "toggle"},
+
+    -- Other
+    -- [300032] = {"Rift Token", "toggle"},
+    [300033] = {"umbrella", "toggle"},
+
+    -- Garbage items
+    --[[[300034] = {"25 Pons", "toggle"},
+    [300035] = {"50 Pons", "toggle"},
+    [300036] = {"100 Pons", "toggle"},
+    [300037] = {"Health Pon", "toggle"},
+
+    -- Traps
+    [300039] = {"Baby Trap", "toggle"},
+    [300040] = {"Laser Trap", "toggle"},
+    [300041] = {"Parade Trap", "toggle"},
+
+    -- DLC1 items
+    [300018] = {"Relic (Cake Stand)", "toggle"},
+    [300019] = {"Relic (Cake)", "toggle"},
+    [300020] = {"Relic (Cake Slice)", "toggle"},
+    [300021] = {"Relic (Shortcake)", "toggle"},
+
+    -- DLC2 items
+    [300022] = {"Relic (Necklace Bust)", "toggle"},
+    [300023] = {"Relic (Necklace)", "toggle"},
+
+    -- Death Wish items
+    [300038] = {"One-Hit Hero Badge", "toggle"},
+    [300042] = {"Camera Badge", "toggle"},
+
+    --Time Pieces
+    [300048] = {"Time Piece (Welcome to Mafia Town)", "toggle"},
+    [300049] = {"Time Piece (Barrel Battle)", "toggle"},
+    [300050] = {"Time Piece (She Came from Outer Space)", "toggle"},
+    [300051] = {"Time Piece (Down with the Mafia!)", "toggle"},
+    [300052] = {"Time Piece (Cheating the Race)", "toggle"},
+    [300053] = {"Time Piece (Heating Up Mafia Town)", "toggle"},
+    [300054] = {"Time Piece (The Golden Vault)", "toggle"},
+    [300055] = {"Time Piece (Time Rift - Sewers)", "toggle"},
+    [300056] = {"Time Piece (Time Rift - Bazaar)", "toggle"},
+    [300057] = {"Time Piece (Time Rift - Mafia of Cooks)", "toggle"},
+
+    [300058] = {"Time Piece (Dead Bird Studio)", "toggle"},
+    [300059] = {"Time Piece (Murder on the Owl Express)", "toggle"},
+    [300060] = {"Time Piece (Train Rush)", "toggle"},
+    [300061] = {"Time Piece (Picture Perfect)", "toggle"},
+    [300062] = {"Time Piece (The Big Parade)", "toggle"},
+    [300063] = {"Time Piece (Award Ceremony)", "toggle"},
+    [300064] = {"Time Piece (Award Ceremony Boss)", "toggle"},
+    [300065] = {"Time Piece (Time Rift - The Owl Express)", "toggle"},
+    [300066] = {"Time Piece (Time Rift - The Moon)", "toggle"},
+    [300067] = {"Time Piece (Time Rift - Dead Bird Studio)", "toggle"},
+
+    [300068] = {"Time Piece (Contractual Obligations)", "toggle"},
+    [300069] = {"Time Piece (The Subcon Well)", "toggle"},
+    [300070] = {"Time Piece (Toilet of Doom)", "toggle"},
+    [300071] = {"Time Piece (Queen Vanessa's Manor)", "toggle"},
+    [300072] = {"Time Piece (Mail Delivery Service)", "toggle"},
+    [300073] = {"Time Piece (Your Contract Has Expired)", "toggle"},
+    [300074] = {"Time Piece (Time Rift - Pipe)", "toggle"},
+    [300075] = {"Time Piece (Time Rift - Village)", "toggle"},
+    [300076] = {"Time Piece (Time Rift - Sleepy Subcon)", "toggle"},
+
+    [300077] = {"Time Piece (The Birdhouse)", "toggle"},
+    [300078] = {"Time Piece (The Lava Cake)", "toggle"},
+    [300079] = {"Time Piece (The Twilight Bell)", "toggle"},
+    [300080] = {"Time Piece (The Windmill)", "toggle"},
+    [300081] = {"Time Piece (The Illness has Spread)", "toggle"},
+    [300082] = {"Time Piece (Time Rift - The Twilight Bell)", "toggle"},
+    [300083] = {"Time Piece (Time Rift - Curly Tail Trail)", "toggle"},
+    [300084] = {"Time Piece (Time Rift - Alpine Skyline)", "toggle"},
+
+    [300085] = {"Time Piece (Time Rift - Gallery)", "toggle"},
+    [300086] = {"Time Piece (Time Rift - The Lab)", "toggle"},
+
+    [300087] = {"Time Piece (Time's End - The Finale)", "toggle"},
+
+    --Contracts
+    [300200] = {"Snatcher's Contract - The Subcon Well", "toggle"},
+    [300201] = {"Snatcher's Contract - Toilet of Doom", "toggle"},
+    [300202] = {"Snatcher's Contract - Queen Vanessa's Manor", "toggle"},
+    [300203] = {"Snatcher's Contract - Mail Delivery Service", "toggle"}
+]]--
+}
---
+++ scripts/autotracking/location_mapping.lua
@@ -0,0 +1,267 @@
+LOCATION_MAPPING = {
+    
+    --[301000] = {"@Spaceship/Spaceship - Rumbi"},
+    --[301001] = {"@Spaceship/Spaceship - Cooking Cat"},
+
+    -- 300000 range - Mafia Town/Batle of the Birds
+    [301002] = {"@Mafia Town/Welcome to Mafia Town/Umbrella"},
+    [302848] = {"@Mafia Town/Red Vault/Yarn"},
+    [304462] = {"@Mafia Town/Dweller Boxes/Yarn"},
+    [303833] = {"@Mafia Town/Old Man 2/Seaside Spaghetti"},
+    [303530] = {"@Mafia Town/Ledge Chest/Yarn"},
+    [303535] = {"@Mafia Town/Yellow Sphere Building Chest/Yarn"},
+    [304456] = {"@Mafia Town/Beneath Scaffolding/Yarn"},
+    [304457] = {"@Mafia Town/On Scaffolding/Yarn"},
+    [304458] = {"@Mafia Town/Plaza Under Boxes/Yarn"},
+    [305572] = {"@Mafia Town/Blue Vault/Brewing Crate"},
+    [304460] = {"@Mafia Town/Small Boat/Yarn"},
+    [304459] = {"@Mafia Town/Cargo Ship/Yarn"},
+    [304463] = {"@Mafia Town/Beach Alcove/Yarn"},
+    [304606] = {"@Mafia Town/Steel Beams/Wood Cage"},
+    [304611] = {"@Mafia Town/Staircase Pon Cluster/Yarn"},
+    [302850] = {"@Mafia Town/Blue Vault/Vault"},
+    [303832] = {"@Mafia Town/Old Man/Yarn"},
+    [304609] = {"@Mafia Town/Palm Tree/Yarn"},
+    [304610] = {"@Mafia Town/Beach Patio/Freestanding"},
+    [304608] = {"@Mafia Town/Steel Beams/Nest"},
+    [304607] = {"@Mafia Town/Top of Ruined Tower/Yarn"},
+    [304831] = {"@Mafia Town/Ice Hat Cage/Yarn"},
+    [304829] = {"@Mafia Town/Hot Air Balloon/Yarn"},
+    [302003] = {"@Mafia Town/Camera Badge/smile :)"},
+    [302004] = {"@Mafia Town/Camera Badge 2/smile :)"},
+    [303489] = {"@Mafia Town/Chest Beneath Aqueduct/Relic"},
+    [305220] = {"@Mafia Town/Secret Cave/Relic"},
+    [303532] = {"@Mafia Town/Crow Chest/Relic"},
+    [305219] = {"@Mafia Town/Port/Relic"},
+    [303534] = {"@Mafia Town/Docks Chest/Relic"},
+    [305218] = {"@Mafia Town/Above Boats/Relic"},
+    [303529] = {"@Mafia Town/Beach Patio/Chest"},
+    [302851] = {"@Mafia Town/Green Vault/Token"},
+    [304214] = {"@Mafia Town/Behind Faucet/Token"},
+    [303483] = {"@Mafia Town/Hidden Buttons/Grab a beach ball"},
+    [303481] = {"@Mafia Town/Clock Tower Chest/Token"},
+    [304213] = {"@Mafia Town/Top of Lighthouse/Token"},
+    [304212] = {"@Mafia Town/Mafia Geek Platform/Token"},
+    [303486] = {"@Mafia Town/Mafia HQ/Behind HQ Chest"},
+
+    [305387] = {"@Mafia Town/Mafia HQ/Hallway Brewing Crate"},
+    [303241] = {"@Mafia Town/Mafia HQ/Freezer Chest"},
+    [304979] = {"@Mafia Town/Mafia HQ/Secret Room"},
+    [303243] = {"@Mafia Town/Mafia HQ/Bathroom Stall Chest"},
+
+    [304874] = {"@Dead Bird Studio/Up the Ladder/Yarn"},
+    [303901] = {"@Dead Bird Studio/DJ Grooves Sign Chest/Yarn"},
+    [305024] = {"@Dead Bird Studio/Red Building Top/Yarn"},
+    [305248] = {"@Dead Bird Studio/Behind Water Tower/Yarn"},
+    [305247] = {"@Dead Bird Studio/Side of House/Yarn"},
+    [303898] = {"@Dead Bird Studio/Tightrope Chest/Relic"},
+    [303899] = {"@Dead Bird Studio/Teepee Chest/Relic"},
+    [303900] = {"@Dead Bird Studio/Conductor Chest/Token"},
+
+    [305313] = {"@Dead Bird Studio/The Conductor's Films/Cafeteria"},
+    [305090] = {"@Dead Bird Studio/The Conductor's Films/Luggage Room Top"},
+    [305091] = {"@Dead Bird Studio/The Conductor's Films/Luggage Room Bottom"},
+    [305701] = {"@Dead Bird Studio/The Conductor's Films/Raven Suite Room"},
+    [305312] = {"@Dead Bird Studio/The Conductor's Films/Raven Suite Top"},
+    [303963] = {"@Dead Bird Studio/The Conductor's Films/Lounge Chest"},
+
+    [304307] = {"@Dead Bird Studio/DJ Groove's Films/Behind Badge Seller"},
+    [304530] = {"@Dead Bird Studio/DJ Groove's Films/Hats Buy Building"},
+
+    [305432] = {"@DBS Basement/Window Platform/Yarn"},
+    [305059] = {"@DBS Basement/Cardboard Conductor/Yarn"},
+    [305057] = {"@DBS Basement/Above Conductor Sign/Yarn"},
+    [305207] = {"@DBS Basement/Logo Wall/Yarn"},
+    [305061] = {"@DBS Basement/Disco Room/Yarn"},
+    [304813] = {"@DBS Basement/Small Room/Token"},
+    [305430] = {"@DBS Basement/Vent Pipe/Yarn"},
+    [305058] = {"@DBS Basement/Tightrope/Yarn"},
+    [305431] = {"@DBS Basement/Cameras/Yarn"},
+    [305819] = {"@DBS Basement/Locked Room/Relic"},
+
+    -- 320000 range - Subcon Forest
+    [324761] = {"@Subcon Forest/Cherry Bomb Bone Cage/Yarn"},
+    [325078] = {"@Subcon Forest/Tree Top Ice Cube/Yarn"},
+    [325077] = {"@Subcon Forest/Graveyard/Ice Cube"},
+    [325471] = {"@Subcon Forest/Graveyard/House Top"},
+    [325469] = {"@Subcon Forest/Ice Cube House/Relic"},
+    [323730] = {"@Subcon Forest/Snatcher Statue Chest/Yarn"},
+    [323729] = {"@Subcon Forest/Stump Platform Chest/Yarn"},
+    [325470] = {"@Subcon Forest/Giant Mushroom Climb/Don't Fall~"},
+    [326296] = {"@Subcon Forest/Swamp Gravestone/Yarn"},
+    [324762] = {"@Subcon Forest/Swamp Near Well/Token"},
+    [324763] = {"@Subcon Forest/Swamp Tree A/Yarn"},
+    [324764] = {"@Subcon Forest/Swamp Tree B/Yarn"},
+    [324706] = {"@Subcon Forest/Swamp Ice Wall/Yarn"},
+    [325468] = {"@Subcon Forest/Swamp Treehouse/Relic"},
+    [323728] = {"@Subcon Forest/Swamp Tree Chest/Yarn"},
+    [324767] = {"@Subcon Forest/Dweller Stump/Yarn"},
+    [324464] = {"@Subcon Forest/Dweller Floating Rocks/Token"},
+    [324709] = {"@Subcon Forest/Dweller Platforming Tree A/Yarn"},
+    [324855] = {"@Subcon Forest/Dweller/Platforming Tree B"},
+    [325473] = {"@Subcon Forest/Giant Time Piece/Relic"},
+    [325472] = {"@Subcon Forest/Gallows/Relic"},
+    [325082] = {"@Subcon Forest/Green and Purple Dweller Rocks/Yarn"},
+    [324463] = {"@Subcon Forest/Dweller/Shack"},
+    [324766] = {"@Subcon Forest/Tall Tree Hookshot Swing/Yarn"},
+    [324710] = {"@Subcon Forest/Burning House/Yarn"},
+    [325079] = {"@Subcon Forest/Burning Tree Climb/Yarn"},
+    [323731] = {"@Subcon Forest/Burning Stump Chest/Token"},
+    [325467] = {"@Subcon Forest/Burning Forest Treehouse/Relic"},
+    [324462] = {"@Subcon Forest/Spider Bone Cage A/Token"},
+    [325080] = {"@Subcon Forest/Spider Bone Cage B/Yarn"},
+    [324765] = {"@Subcon Forest/Triple Spider Bounce/Yarn"},
+    [324856] = {"@Subcon Forest/Noose Treehouse/Relic"},
+    [324465] = {"@Subcon Forest/Ice Cube Shack/Token"},
+    [323734] = {"@Subcon Forest/Long Tree Climb Chest/Yarn"},
+    [323735] = {"@Subcon Forest/Boss Arena Chest/Yarn"},
+    [324114] = {"@Subcon Forest/Subcon Well/Hookshot Badge Chest"},
+    [324612] = {"@Subcon Forest/Subcon Well/Above Chest"},
+    [324311] = {"@Subcon Forest/Subcon Well/On Pipe"},
+    [325318] = {"@Subcon Forest/Subcon Well/Mushroom"},
+    [325466] = {"@Subcon Forest/Queen Vanessa's Manor/Rooftop"},
+    [324841] = {"@Subcon Forest/Queen Vanessa's Manor/Cellar"},
+    [323808] = {"@Subcon Forest/Queen Vanessa's Manor/Bedroom Chest"},
+    [323896] = {"@Subcon Forest/Queen Vanessa's Manor/Hall Chest"},
+    [325546] = {"@Subcon Forest/Queen Vanessa's Manor/Chandelier"},
+
+    --[[
+    -- 330000 range - Alpine Skyline
+    [334856] = {"@Goat Village/Alpine Skyline - Goat Village: Below Hookpoint"},
+    [334855] = {"@Goat Village/Alpine Skyline - Goat Village: Hidden Branch"},
+    [333635] = {"@Alpine Free Roam/Alpine Skyline - Goat Refinery"},
+    [335911] = {"@Alpine Free Roam/Alpine Skyline - Bird Pass Fork"},
+    [335756] = {"@Alpine Free Roam/Alpine Skyline - Yellow Band Hills"},
+    [335561] = {"@Alpine Free Roam/Alpine Skyline - The Purrloined Village: Horned Stone"},
+    [334831] = {"@Alpine Free Roam/Alpine Skyline - The Purrloined Village: Chest Reward"},
+    [334758] = {"@The Birdhouse/Alpine Skyline - The Birdhouse: Triple Crow Chest"},
+    [336497] = {"@The Birdhouse/Alpine Skyline - The Birdhouse: Dweller Platforms Relic"},
+    [336496] = {"@The Birdhouse/Alpine Skyline - The Birdhouse: Brewing Crate House"},
+    [335885] = {"@The Birdhouse/Alpine Skyline - The Birdhouse: Hay Bale"},
+    [335886] = {"@The Birdhouse/Alpine Skyline - The Birdhouse: Alpine Crow Mini-Gauntlet"},
+    [335492] = {"@The Birdhouse/Alpine Skyline - The Birdhouse: Outer Edge"},
+
+    [337058] = {"@Alpine Free Roam/Alpine Skyline - Mystifying Time Mesa: Zipline"},
+    [336052] = {"@Alpine Free Roam/Alpine Skyline - Mystifying Time Mesa: Gate Puzzle"},
+    [336311] = {"@Alpine Free Roam/Alpine Skyline - Ember Summit"},
+    [335448] = {"@The Lava Cake/Alpine Skyline - The Lava Cake: Center Fence Cage"},
+    [334291] = {"@The Lava Cake/Alpine Skyline - The Lava Cake: Outer Island Chest"},
+    [335417] = {"@The Lava Cake/Alpine Skyline - The Lava Cake: Dweller Pillars"},
+    [335418] = {"@The Lava Cake/Alpine Skyline - The Lava Cake: Top Cake"},
+    [334434] = {"@Alpine Free Roam/Alpine Skyline - The Twilight Path"},
+    [336478] = {"@The Twilight Bell/Alpine Skyline - The Twilight Bell: Wide Purple Platform"},
+    [335826] = {"@The Twilight Bell/Alpine Skyline - The Twilight Bell: Ice Platform"},
+    [334760] = {"@Alpine Free Roam/Alpine Skyline - Goat Outpost Horn"},
+    [334776] = {"@Alpine Free Roam/Alpine Skyline - Windy Passage"},
+    [336395] = {"@The Windmill/Alpine Skyline - The Windmill: Time Trial"},
+    [335783] = {"@The Windmill/Alpine Skyline - The Windmill: Entrance"},
+    [335815] = {"@The Windmill/Alpine Skyline - The Windmill: Dropdown"},
+    [335389] = {"@The Windmill/Alpine Skyline - The Windmill: House Window"},
+
+    [304108] = {"@Time's End/Time's End - Frozen Item"},
+
+    -- 310000 range - Act Completions
+    [312758] = {"@Time Rift - Gallery/Act Completion (Time Rift - Gallery)"},
+    [312838] = {"@Time Rift - The Lab/Act Completion (Time Rift - The Lab)"},
+]]--
+
+    [311771] = {"@Mafia Town/Welcome to Mafia Town/Time Piece"},
+    [311958] = {"@Mafia Town/Barrel Battle/Time Piece"},
+    [312262] = {"@Mafia Town/Mafia Geek/She Came from Outer Space"},
+    [311326] = {"@Mafia Town/Mafia HQ/Time Piece"},
+    [312318] = {"@Mafia Town/Beach Alcove/Cheating the Race"},
+    [311481] = {"@Mafia Town/Mafia Geek/Heating Up Mafia Town"},
+    [312250] = {"@Mafia Town/The Golden Vault/Time Piece"},
+    [312465] = {"@Mafia Town/Bazaar/Time Rift"},
+    [312484] = {"@Mafia Town/Sewers/Time Rift"},
+    [311855] = {"@Mafia Town/Mafia of Cooks/Time Rift"},
+
+    [311383] = {"@Dead Bird Studio/Act Completion/Dead Bird Studio"},
+    [311544] = {"@Dead Bird Studio/The Conductor's Films/Murder on the Owl Express"},
+    [311587] = {"@Dead Bird Studio/DJ Groove's Films/Picture Perfect"},
+    [312481] = {"@Dead Bird Studio/The Conductor's Films/Train Rush"},
+    [311157] = {"@Dead Bird Studio/DJ Groove's Films/The Big Parade"},
+    [311488] = {"@Dead Bird Studio/Act Completion/Award Ceremony"},
+    [312253] = {"@DBS Basement/Act Completion/Time Piece"},
+    [312807] = {"@Dead Bird Studio/The Conductor's Films/Time Rift"},
+    [312785] = {"@Dead Bird Studio/DJ Groove's Films/Time Rift"},
+    [312577] = {"@Dead Bird Studio/Dead Bird Studio (Rift)/Time Piece"},
+
+    [312317] = {"@Subcon Forest/Snatcher/Contractual Obligations"},
+    [311160] = {"@Subcon Forest/Subcon Well/Time Piece"},
+    [311984] = {"@Subcon Forest/Boss Arena/Toilet of Doom"},
+    [312017] = {"@Subcon Forest/Queen Vanessa's Manor/Time Piece"},
+    [312032] = {"@Subcon Forest/Snatcher/Mail Delivery Service"},
+    [311390] = {"@Subcon Forest/Boss Arena/Your Contract has Expired"},
+
+    [313069] = {"@Subcon Forest/Pipe/Time Rift"},
+    [313056] = {"@Subcon Forest/Village/Time Rift"},
+
+    [312086] = {"@Subcon Forest/Sleepy Subcon/Time Rift"},
+
+    --[[
+    [311428] = {"@The Birdhouse/Act Completion (The Birdhouse)"},
+    [312509] = {"@The Lava Cake/Act Completion (The Lava Cake)"},
+    [311540] = {"@The Twilight Bell/Act Completion (The Twilight Bell)"},
+    [312263] = {"@The Windmill/Act Completion (The Windmill)"},
+    [312022] = {"@The Illness has Spread/Act Completion (The Illness has Spread)"},
+    [312399] = {"@Time Rift - The Twilight Bell/Act Completion (Time Rift - The Twilight Bell)"},
+    [313335] = {"@Time Rift - Curly Tail Trail/Act Completion (Time Rift - Curly Tail Trail)"},
+    [311777] = {"@Time Rift - Alpine Skyline/Act Completion (Time Rift - Alpine Skyline)"},
+
+    [311872] = {"@Time's End/Act Completion (Time's End - The Finale)"},
+]]--
+
+    -- Storybook pages
+    [345091] = {"@Mafia Town/Mafia of Cooks/Pages"}, --Fish Pile"
+    [345090] = {"@Mafia Town/Mafia of Cooks/Pages"}, --Trash Mound"
+    [345092] = {"@Mafia Town/Mafia of Cooks/Pages"}, --Beside Red Building"
+    [345095] = {"@Mafia Town/Mafia of Cooks/Pages"}, --Behind Shipping Containers"
+    [345093] = {"@Mafia Town/Mafia of Cooks/Pages"}, --Top of Boat"
+    [345094] = {"@Mafia Town/Mafia of Cooks/Pages"}, --Below Dock"
+
+    [345449] = {"@Dead Bird Studio/Dead Bird Studio (Rift)/Pages"}, -- Behind Cardboard Planet
+    [345447] = {"@Dead Bird Studio/Dead Bird Studio (Rift)/Pages"}, --Near Time Rift Gate"
+    [345448] = {"@Dead Bird Studio/Dead Bird Studio (Rift)/Pages"}, --Top of Metal Bar"
+    [345450] = {"@Dead Bird Studio/Dead Bird Studio (Rift)/Pages"}, --Lava Lamp"
+    [345451] = {"@Dead Bird Studio/Dead Bird Studio (Rift)/Pages"}, --Above Horse Picture"
+    [345452] = {"@Dead Bird Studio/Dead Bird Studio (Rift)/Pages"}, --Green Screen"
+    [345453] = {"@Dead Bird Studio/Dead Bird Studio (Rift)/Pages"}, --In The Corner"
+    [345445] = {"@Dead Bird Studio/Dead Bird Studio (Rift)/Pages"}, --Above TV Room"
+
+    [345373] = {"@Subcon Forest/Sleepy Subcon/Pages"}, --Behind Entrance Area"
+    [345327] = {"@Subcon Forest/Sleepy Subcon/Pages"}, --Near Wrecking Ball"
+    [345371] = {"@Subcon Forest/Sleepy Subcon/Pages"}, --Behind Crane"
+    [345326] = {"@Subcon Forest/Sleepy Subcon/Pages"}, --Wrecked Treehouse"
+    [345372] = {"@Subcon Forest/Sleepy Subcon/Pages"}, --Behind 2nd Rift Gate"
+    [345328] = {"@Subcon Forest/Sleepy Subcon/Pages"}, --Rotating Platform"
+    [345329] = {"@Subcon Forest/Sleepy Subcon/Pages"}, --Behind 3rd Rift Gate"
+    [345330] = {"@Subcon Forest/Sleepy Subcon/Pages"}, --Frozen Tree"
+    [345370] = {"@Subcon Forest/Sleepy Subcon/Pages"}, --Secret Library"
+--[[
+    [345016] = {"@Time Rift - Alpine Skyline/Alpine Skyline (Rift) - Page: Entrance Area Hidden Ledge"},
+    [345012] = {"@Time Rift - Alpine Skyline/Alpine Skyline (Rift) - Page: Windmill Island Ledge"},
+    [345015] = {"@Time Rift - Alpine Skyline/Alpine Skyline (Rift) - Page: Waterfall Wooden Pillar"},
+    [345014] = {"@Time Rift - Alpine Skyline/Alpine Skyline (Rift) - Page: Lonely Birdhouse Top"},
+    [345013] = {"@Time Rift - Alpine Skyline/Alpine Skyline (Rift) - Page: Below Aqueduct"},
+]]--
+    -- Contracts
+    [300200] = {"@Subcon Forest/Snatcher/Contracts"}, --Snatcher's Contract - The Subcon Well"
+    [300201] = {"@Subcon Forest/Snatcher/Contracts"}, --Snatcher's Contract - Toilet of Doom"
+    [300202] = {"@Subcon Forest/Snatcher/Contracts"}, --Snatcher's Contract - Queen Vanessa's Manor"
+    [300203] = {"@Subcon Forest/Snatcher/Contracts"}, --Snatcher's Contract - Mail Delivery Service"
+--[[
+    -- shop_locations
+    [301003] = {"@Badge Seller/Badge Seller - Item 1"},
+    [301004] = {"@Badge Seller/Badge Seller - Item 2"},
+    [301005] = {"@Badge Seller/Badge Seller - Item 3"},
+    [301006] = {"@Badge Seller/Badge Seller - Item 4"},
+    [301007] = {"@Badge Seller/Badge Seller - Item 5"},
+    [301008] = {"@Badge Seller/Badge Seller - Item 6"},
+    [301009] = {"@Badge Seller/Badge Seller - Item 7"},
+    [301010] = {"@Badge Seller/Badge Seller - Item 8"},
+    [301011] = {"@Badge Seller/Badge Seller - Item 9"},
+    [301012] = {"@Badge Seller/Badge Seller - Item 10"},
+    [301013] = {"@Spaceship/Mafia Boss Shop Item"}
+]]--
+}
