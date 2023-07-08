240c95007eea0d9e76e882e005b7542787ff799b
Hunter Oppel <hun.oppel@gmail.com>
2023-07-08 14:55:26 -0400

    Added yarn and timepieces to tracker

--- scripts/autotracking/item_mapping.lua
+++ scripts/autotracking/item_mapping.lua
@@ -5,7 +5,8 @@ ITEM_MAPPING = {
     [300001] = {"yarn", "consumable"},
 
     -- Relics
-    --[[[300006] = {"Relic (Burger Patty)", "toggle"},
+--[[
+    [300006] = {"Relic (Burger Patty)", "toggle"},
     [300007] = {"Relic (Burger Cushion)", "toggle"},
     [300008] = {"Relic (Mountain Set)", "toggle"},
     [300009] = {"Relic (Train)", "toggle"},
@@ -16,7 +17,8 @@ ITEM_MAPPING = {
     [300014] = {"Relic (Crayon Box)", "toggle"},
     [300015] = {"Relic (Red Crayon)", "toggle"},
     [300016] = {"Relic (Blue Crayon)", "toggle"},
-    [300017] = {"Relic (Green Crayon)", "toggle"},]]--
+    [300017] = {"Relic (Green Crayon)", "toggle"},
+]]--
 
     -- Badges
     -- [300024] = {"Projectile Badge", "toggle"},
@@ -57,54 +59,54 @@ ITEM_MAPPING = {
     -- Death Wish items
     [300038] = {"One-Hit Hero Badge", "toggle"},
     [300042] = {"Camera Badge", "toggle"},
-
+]]--
     --Time Pieces
-    [300048] = {"Time Piece (Welcome to Mafia Town)", "toggle"},
-    [300049] = {"Time Piece (Barrel Battle)", "toggle"},
-    [300050] = {"Time Piece (She Came from Outer Space)", "toggle"},
-    [300051] = {"Time Piece (Down with the Mafia!)", "toggle"},
-    [300052] = {"Time Piece (Cheating the Race)", "toggle"},
-    [300053] = {"Time Piece (Heating Up Mafia Town)", "toggle"},
-    [300054] = {"Time Piece (The Golden Vault)", "toggle"},
-    [300055] = {"Time Piece (Time Rift - Sewers)", "toggle"},
-    [300056] = {"Time Piece (Time Rift - Bazaar)", "toggle"},
-    [300057] = {"Time Piece (Time Rift - Mafia of Cooks)", "toggle"},
+    [300048] = {"timepiece", "consumable"}, -- Welcome to Mafia Town
+    [300049] = {"timepiece", "consumable"}, -- Barrel Battle
+    [300050] = {"timepiece", "consumable"}, --She Came from Outer Space
+    [300051] = {"timepiece", "consumable"}, -- Down with the Mafia!
+    [300052] = {"timepiece", "consumable"}, -- Cheating the Race
+    [300053] = {"timepiece", "consumable"}, -- Heating Up Mafia Town
+    [300054] = {"timepiece", "consumable"}, -- The Golden Vault
+    [300055] = {"timepiece", "consumable"}, -- Time Rift - Sewers
+    [300056] = {"timepiece", "consumable"}, -- Time Rift - Bazaar
+    [300057] = {"timepiece", "consumable"}, -- Time Rift - Mafia of Cooks
 
-    [300058] = {"Time Piece (Dead Bird Studio)", "toggle"},
-    [300059] = {"Time Piece (Murder on the Owl Express)", "toggle"},
-    [300060] = {"Time Piece (Train Rush)", "toggle"},
-    [300061] = {"Time Piece (Picture Perfect)", "toggle"},
-    [300062] = {"Time Piece (The Big Parade)", "toggle"},
-    [300063] = {"Time Piece (Award Ceremony)", "toggle"},
-    [300064] = {"Time Piece (Award Ceremony Boss)", "toggle"},
-    [300065] = {"Time Piece (Time Rift - The Owl Express)", "toggle"},
-    [300066] = {"Time Piece (Time Rift - The Moon)", "toggle"},
-    [300067] = {"Time Piece (Time Rift - Dead Bird Studio)", "toggle"},
+    [300058] = {"timepiece", "consumable"}, -- Dead Bird Studio
+    [300059] = {"timepiece", "consumable"}, -- Murder on the Owl Express
+    [300060] = {"timepiece", "consumable"}, -- Train Rush
+    [300061] = {"timepiece", "consumable"}, -- Picture Perfect
+    [300062] = {"timepiece", "consumable"}, -- The Big Parade
+    [300063] = {"timepiece", "consumable"}, -- Award Ceremony
+    [300064] = {"timepiece", "consumable"}, -- Dead Bird Studio Basement
+    [300065] = {"timepiece", "consumable"}, -- Time Rift - The Owl Express
+    [300066] = {"timepiece", "consumable"}, -- Time Rift - The Moon
+    [300067] = {"timepiece", "consumable"}, -- Time Rift - Dead Bird Studio
 
-    [300068] = {"Time Piece (Contractual Obligations)", "toggle"},
-    [300069] = {"Time Piece (The Subcon Well)", "toggle"},
-    [300070] = {"Time Piece (Toilet of Doom)", "toggle"},
-    [300071] = {"Time Piece (Queen Vanessa's Manor)", "toggle"},
-    [300072] = {"Time Piece (Mail Delivery Service)", "toggle"},
-    [300073] = {"Time Piece (Your Contract Has Expired)", "toggle"},
-    [300074] = {"Time Piece (Time Rift - Pipe)", "toggle"},
-    [300075] = {"Time Piece (Time Rift - Village)", "toggle"},
-    [300076] = {"Time Piece (Time Rift - Sleepy Subcon)", "toggle"},
+    [300068] = {"timepiece", "consumable"}, -- Contractual Obligations
+    [300069] = {"timepiece", "consumable"}, -- The Subcon Well
+    [300070] = {"timepiece", "consumable"}, -- Toilet of Doom
+    [300071] = {"timepiece", "consumable"}, -- Queen Vanessa's Manor
+    [300072] = {"timepiece", "consumable"}, -- Mail Delivery Service
+    [300073] = {"timepiece", "consumable"}, -- Your Contract Has Expired
+    [300074] = {"timepiece", "consumable"}, -- Time Rift - Pipe
+    [300075] = {"timepiece", "consumable"}, -- Time Rift - Village
+    [300076] = {"timepiece", "consumable"}, -- Time Rift - Sleepy Subcon
 
-    [300077] = {"Time Piece (The Birdhouse)", "toggle"},
-    [300078] = {"Time Piece (The Lava Cake)", "toggle"},
-    [300079] = {"Time Piece (The Twilight Bell)", "toggle"},
-    [300080] = {"Time Piece (The Windmill)", "toggle"},
-    [300081] = {"Time Piece (The Illness has Spread)", "toggle"},
-    [300082] = {"Time Piece (Time Rift - The Twilight Bell)", "toggle"},
-    [300083] = {"Time Piece (Time Rift - Curly Tail Trail)", "toggle"},
-    [300084] = {"Time Piece (Time Rift - Alpine Skyline)", "toggle"},
+    [300077] = {"timepiece", "consumable"}, -- The Birdhouse
+    [300078] = {"timepiece", "consumable"}, -- The Lava Cake
+    [300079] = {"timepiece", "consumable"}, -- The Twilight Bell
+    [300080] = {"timepiece", "consumable"}, -- The Windmill
+    [300081] = {"timepiece", "consumable"}, -- The Illness has Spread
+    [300082] = {"timepiece", "consumable"}, -- Time Rift - The Twilight Bell
+    [300083] = {"timepiece", "consumable"}, -- Time Rift - Curly Tail Trail
+    [300084] = {"timepiece", "consumable"}, -- Time Rift - Alpine Skyline
 
-    [300085] = {"Time Piece (Time Rift - Gallery)", "toggle"},
-    [300086] = {"Time Piece (Time Rift - The Lab)", "toggle"},
+    [300085] = {"timepiece", "consumable"}, -- Time Rift - Gallery
+    [300086] = {"timepiece", "consumable"}, -- Time Rift - The Lab
 
-    [300087] = {"Time Piece (Time's End - The Finale)", "toggle"},
-
+    [300087] = {"timepiece", "consumable"}, -- Time's End - The Finale
+--[[
     --Contracts
     [300200] = {"Snatcher's Contract - The Subcon Well", "toggle"},
     [300201] = {"Snatcher's Contract - Toilet of Doom", "toggle"},
